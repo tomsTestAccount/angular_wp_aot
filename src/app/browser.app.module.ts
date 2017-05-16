6f8a8ae4a091930795696ce3367ca4aa2b06eaaf
Thomas Bergmann <t.bergmann@reliatec.de>
2017-05-16 16:46:01 +0200

    update files for new config-version

--- src/app/browser.app.module.ts
+++ src/app/browser.app.module.ts
@@ -2,13 +2,13 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-//import { AppLoginComponent } from './appLogin.component';
+//import { AppComponent } from './app.component';
+import { AppLoginComponent } from './appLogin.component';
 import { AppModule } from './app.module';
 import { BrowserTransferStateModule } from '../modules/transfer-state/browser-transfer-state.module';
 
 @NgModule({
-  bootstrap: [AppComponent],
+  bootstrap: [AppLoginComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule.withServerTransition({
